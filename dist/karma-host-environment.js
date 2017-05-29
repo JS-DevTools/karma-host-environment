154ddad2839eba86463ac3db7d7497eca5b1b133
James Messinger <james.messinger@gmail.com>
2017-05-29 09:02:07 -0500

    Added a `toJSON()` method so `JSON.stringify()` can safely serialize the `host` object.  This is necessary because `host.global` contains circular references.

--- dist/karma-host-environment.js
+++ dist/karma-host-environment.js
@@ -9,18 +9,67 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.host = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var Host = require('./host');
 var userAgent = require('./user-agent');
 
-module.exports = window.host = {
+module.exports = window.host = new Host({
   global: window,
   node: false,
   karma: false,
   os: userAgent.getOS(),
   browser: userAgent.getBrowser(),
   env: {},
+});
+
+},{"./host":2,"./user-agent":3}],2:[function(require,module,exports){
+'use strict';
+
+module.exports = Host;
+
+/**
+ * Returns a Host insance with the given properties.
+ *
+ * @param {object} props - The host environment properties
+ * @class
+ */
+function Host (props) {
+  clone(this, props);
+}
+
+/**
+ * Safely serializes a Host object as JSON.
+ *
+ * @returns {object}
+ */
+Host.prototype.toJSON = function () {
+  var json = clone({}, this);
+
+  // The global object can't be serialized to JSON because it has circular references.
+  // So call just serialize its toString() representation instead
+  json.global = Object.prototype.toString.call(json.global);
+
+  return json;
 };
 
-},{"./user-agent":2}],2:[function(require,module,exports){
+/**
+ * Copies the properties of one object to another.
+ *
+ * @param {object} target - The object that the properties are copied to
+ * @param {object} source - The object that the properties are copied from
+ * @returns {object} - Returns the target object
+ */
+function clone (target, source) {
+  var keys = Object.keys(source);
+
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+},{}],3:[function(require,module,exports){
 'use strict';
 
 module.exports = {
